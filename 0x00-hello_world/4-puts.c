1985bd0a9c703482d6705e87ca8e080750baeb62
ndegz <ndegz@example.com>
2022-09-08 15:44:58 -0700

    puts function in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-int main (){
+/*function that uses puts function in C */
+int main() {
 
 	char myStr[] = " \"Programming is like building a multilingual puzzle";
 
 	puts(myStr);
-	return 0;
+	{return 0};
 }
