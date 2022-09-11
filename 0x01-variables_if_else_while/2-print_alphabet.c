9eb030c870bf8bf26199880c697ac22ca258412b
ndegz <ndegz@example.com>
2022-09-11 06:58:13 -0700

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function prints lowercase alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
