704d6d99fdb99b012023bc90cb3e8e2e5f3f8573
ndegz <ndegz@example.com>
2022-09-12 00:45:48 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function that prints numbers below 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+
+		i++;
+	}
+	return (0);
+}
