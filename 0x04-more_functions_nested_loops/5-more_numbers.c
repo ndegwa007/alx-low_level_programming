386b5205ae6ddf11a6bacaaeda988a21a8c43710
ndegz <ndegz@example.com>
2022-09-15 07:03:29 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14 ten times
+ *
+ * Return: 0-14 x10
+ */
+
+void more_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+
+		int i = 0;
+
+		while (i <= 14)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+			i++;
+		}
+		_putchar('\n');
+		n++;
+	}
+
+}
