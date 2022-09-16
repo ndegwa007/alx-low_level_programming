330165bcc3326f8b0983ecc0a86a57394fca1441
ndegz <ndegz@example.com>
2022-09-16 07:29:02 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,16 +15,21 @@ int main(void)
 	char str[] = "Fizz";
 
 	char strb[] = "Buzz";
+	char strfb[] = "FizzBuzz";
 
 	for (j = 1; j <= i; j++)
 	{
-		if (j % 3 == 0)
+		if ((j % 3 == 0) && (j % 5 == 0))
 		{
-			printf("%s ", str);
+			printf("%s ", strfb);
 		}
 		else if (j % 5 == 0)
 		{
 			printf("%s ", strb);
+		}
+		else if (j % 3 == 0)
+		{
+			printf("%s ", str);
 		}
 		else
 		{
