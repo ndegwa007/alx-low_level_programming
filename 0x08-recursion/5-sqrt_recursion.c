9dfdf244e14ef09e358e56de95fb3f6a45d5d12e
ndegz <ndegz@example.com>
2022-09-29 00:56:04 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns sqrt of num
+ * @n: number int
+ *
+ * Return: sqrt
+ **/
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt_num(n, 1));
+}
+
+/**
+ * sqrt_num - get sqrt recursively
+ * @x: int
+ * @y: int
+ *
+ * Return: -1 or sqrt of x
+ **/
+
+int sqrt_num(int x, int y)
+{
+	int s = y * y;
+
+	if (s > x)
+		return (-1);
+	if (s == x)
+		return (y);
+	if (s < x)
+		return (sqrt_num(x, y + 1));
+}
