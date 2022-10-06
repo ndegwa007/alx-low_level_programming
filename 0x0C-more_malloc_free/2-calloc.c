b30da9b93389375d789fcd7099cb78a351473776
ndegz <ndegz@example.com>
2022-10-06 15:24:11 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if ((nmemb ==  0) || (size == 0))
 		return (NULL);
-	if (!p)
+	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
 		p[i] = 0;
