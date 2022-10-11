e7fc87af0540dbadcf54dcee2579759e6e39b6d7
ndegz <ndegz@example.com>
2022-10-11 12:07:58 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - dog details
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name 
+ *
+ * Description: dog data
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
