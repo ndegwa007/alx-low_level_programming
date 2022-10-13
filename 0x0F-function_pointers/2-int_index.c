f34f4deabeaee677da87aea0428c6af01f6779e8
ndegz <ndegz@example.com>
2022-10-13 07:59:17 -0700

    pointer to a function that checks for int on array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	bool x;
 
-	if (array != NULL && size >= 0 && cmp != NULL)
+	if (array != NULL && size > 0 && cmp != NULL)
 	{
 		if (size  <= 0)
 		{
@@ -30,4 +30,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 			}
 		}
 	}
+	return (-1);
 }
