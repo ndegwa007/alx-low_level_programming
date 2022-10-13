a14397f4959e5e87c5dd79253aefed4049f7cd20
ndegz <ndegz@example.com>
2022-10-13 05:40:36 -0700

    pointer to function returning name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function prints name
+ * @name: name
+ * @f: pointer to function printing name
+ * Return: none
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
