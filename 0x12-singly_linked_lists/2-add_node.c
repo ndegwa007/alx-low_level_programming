7b27f9a4cb525ceb996ba64ffc00f6a2f28558ee
ndegz <ndegz@example.com>
2022-10-24 07:05:46 -0700

    length and string print

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include "string.h"
+#include <stdlib.h>
+
+/**
+ * add_node - add a new node at the beginning of a linked lists
+ * @head: double pointer to the first node
+ * @str: string to be added to the linked list
+ *
+ * Return: address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+		return (NULL);
+	ptr->str = strdup(str);
+
+	if (ptr->str == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->len = strlen(ptr->str);
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
+}
