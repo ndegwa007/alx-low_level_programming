f874e4d9788ae4a9d4107eaff99a064fea96dc20
ndegz <ndegz@example.com>
2022-10-27 00:53:49 -0700

    items count and print

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all elements of a listint_t
+ * @h: pointer node head
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+## More On Singly Linked Lists
+
+### A folder with various functions on linked lists in C
+
+1. size\_t print\_listint(const listint\_t *h);
+
