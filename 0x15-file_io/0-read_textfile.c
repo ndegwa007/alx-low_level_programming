d1eb2cc81dd1436eb15548c1b74a7ad8cd50daf0
ndegz <ndegz@example.com>
2022-11-04 09:39:55 -0700

    read a file text and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file
+ * @filename: name of the file
+ * @letters: number of letters to read and print
+ *
+ * Return: returns the actual number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t o, w, r;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		exit(1);
+	}
+	free(buffer);
+	close(o);
+
+	return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,7 @@
+## File I/O operations in C.
+
+Functions showing various file input and output operations in C.
+
+1. 0-read\_textfile.c 
+> a function that reads a textfile and prints it to the POSIX standard output.
+
