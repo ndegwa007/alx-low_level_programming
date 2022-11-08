5ed4aa893d498be19f7c63e63aa40ef32564e26c
ndegz <ndegz@example.com>
2022-11-08 03:24:07 -0800

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,23 @@ unsigned int binary_to_uint(const char *b)
 {
 	int x, decimal = 0, base = 1;
 
-	int slen = strlen(b);
-
-
-	for (x = (slen - 1); x >= 0; x--)
+	if (b == NULL)
+		return (0);
+	for (x = strlen(b) - 1; x >= 0; x--)
 	{
+
 		if (b[x] == '1')
 		{
 			decimal += base;
 		}
 		base *= 2;
+	}
+
+	while (b[x] != '\0')
+	{
+		if (b[x] != '1' || b[x] != '0')
+			return (0);
+		x += 1;
 	}
 
 	return (decimal);
